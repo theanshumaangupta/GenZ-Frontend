98e4339d9b94d157deee71918368ec21485cb390
unknown <theanshumangupta@gmail.com>
2025-09-28 05:49:51 +0530

    animation For n

--- src/components/TextScroll.jsx
+++ src/components/TextScroll.jsx
@@ -5,15 +5,28 @@ export default function TextScroll() {
     const textRef = useRef(null);
 
     useEffect(() => {
+        function wobble(progress) {
+            const MoveY = 250; // how much to move up initially
+            if (progress < 0.6) {
+                // -200 → 0 in 60%
+                return -MoveY + (progress / 0.6) * MoveY;
+            } else if (progress < 0.8) {
+                // 0 → 40 in next 20%
+                return ((progress - 0.6) / 0.2) * 40;
+            } else {
+                // 40 → 0 in last 20%
+                return 40 - ((progress - 0.8) / 0.2) * 40;
+            }
+        }
+
         function handleScroll() {
             if (boxRef.current && textRef.current) {
                 const rect = boxRef.current.getBoundingClientRect();
                 const windowHeight = window.innerHeight;
                 // total scrollable distance
-                const totalScroll = rect.height - (windowHeight);
-                console.log(rect.height, windowHeight, totalScroll);
-                // subtracting 0px becos of the pre start of movement
-                const scrolled = Math.min(Math.max(-(rect.top ), 0), totalScroll);
+                const totalScroll = rect.height - (windowHeight - 500);
+                // subtracting 5000px becos of the pre start of movement
+                const scrolled = Math.min(Math.max(-(rect.top - 500), 0), totalScroll);
 
                 // creating progress/ratio type for percentage 0 to 1
                 const progress = scrolled / totalScroll;
@@ -22,6 +35,15 @@ export default function TextScroll() {
                 const moveX = 80 - progress * 150; // start 80% → end -70%
 
                 textRef.current.style.transform = `translateX(${moveX}%)`;
+
+                const MoveYSpeed = 2
+                document.getElementsByClassName("up")[0].style.setProperty('--progress', `${wobble(Math.min(progress * MoveYSpeed, 1))}px`);
+                const rotateSpeed = 4
+                const rotateDegree = 90;
+                document.getElementsByClassName("up")[0].style.setProperty('--progressTurn', `${Math.max((rotateDegree - (progress * rotateSpeed * rotateDegree)), 0)}deg`);
+                const scaleSpeed = 4
+                const startScale = 0.9
+                document.getElementsByClassName("up")[0].style.setProperty('--ratio', `${Math.min(Math.max(progress * scaleSpeed, startScale), 1)}`);
             }
         }
 
@@ -38,9 +60,9 @@ export default function TextScroll() {
     return (
         <section ref={boxRef} className="bg-primary h-[500vh] w-full">
             <div className="sticky overflow-hidden top-0 h-screen flex flex-col justify-evenly items-center">
-                <div ref={textRef} className="moving-text h1-div overflow-hidden">
-                    <h1 className="h1-text whitespace-nowrap text-9xl font-[1000] font-sans z-10 relative">
-                        we wanna be where the people are
+                <div ref={textRef} className="moving-text h1-div">
+                    <h1 className="h1-text whitespace-nowrap text-[10rem] font-[1000] font-sans z-10 relative">
+                        we wan<div className="up inline-block">n</div>a be where the people are
                     </h1>
                 </div>
                 <p>
