e1be9f675374402f0e9d418e5d1158d1702cb043
unknown <theanshumangupta@gmail.com>
2025-10-07 06:34:22 +0530

    cardsAnimation

--- src/components/CallUs.jsx
+++ src/components/CallUs.jsx
@@ -5,91 +5,102 @@ import { useOnScreen } from "./useOnScreen"
 
 export default function CallUs() {
   const needRef = useRef();
-    const isVisible = useOnScreen(needRef, "0px");
- const heaartRef = useRef();
-    const isVisible2 = useOnScreen(heaartRef, "0px");
+  const isVisible = useOnScreen(needRef, "0px");
+  const heaartRef = useRef();
+  const isVisible2 = useOnScreen(heaartRef, "0px");
   return (
     <div data-nav-bg="dark" className="bg-primary w-full min-h-screen ">
       <h1 className=" text-3xl md:text-7xl font-[1000] text-black text-center max-w-[70rem] mx-auto py-20 lg:pt-20 lg:pb-0">
         call us if you
         <div className="relative inline font-lora font-[40] italic  ">
-          <img src="assets/double-underline.svg" className={`absolute -bottom-10 left-0 h-full scale-animation ${isVisible ? "scale-100 " : "scale-0 "}`}  style={{ transitionDelay: "0.9s" }} alt="" />
+          <img src="assets/double-underline.svg" className={`absolute -bottom-10 left-0 h-full scale-animation ${isVisible ? "scale-100 " : "scale-0 "}`} style={{ transitionDelay: "0.9s" }} alt="" />
           &nbsp;
-          <div ref={needRef} className={`inline-block scale-animation ${isVisible ? "scale-100 " : "scale-0 "}`}  style={{ transitionDelay: "0.5s" }}>
+          <div ref={needRef} className={`inline-block scale-animation ${isVisible ? "scale-100 " : "scale-0 "}`} style={{ transitionDelay: "0.5s" }}>
             need:
           </div>
           &nbsp;
         </div>
       </h1>
       <div className="flex w-[80%] lg:w-fit flex-col relative flex-wrap md:flex-row items-center justify-center mx-auto gap-10 md:-space-x-10 lg:my-20 md:py-20 lg:px-10 xl:px-10">
-        <div className="bg-[#2d7864] sticky top-40 text-white md:relative md:top-0   rotate-10 min-w-[15rem] min-h-[20rem] md:w-[17rem] lg:w-[15rem] lg:h-[20rem] md:h-[40vw] flex flex-col justify-center items-start gap-4 p-5 rounded-2xl " alt="" >
-          <img src="assets/pencil-camera.svg" className="absolute left-[50%] translate-[-50%] top-0 w-30" alt="" />
-          <h1 className="text-4xl font-[1000]">brand</h1>
-          <div className="h-1 w-full bg-black"></div>
-          <ul className="flex flex-col gap-2 text-xl">
-            <li>Brand Strategy</li>
-            <li>360 creativity</li>
-            <li>Art Direction</li>
-            <li>Copywriting</li>
-            <li>Editing</li>
-          </ul>
+        <div className="cards">
+          <div className=" bg-[#2d7864] sticky top-40 text-white md:relative md:top-0   rotate-10 min-w-[15rem] min-h-[20rem] md:w-[17rem] lg:w-[15rem] lg:h-[20rem] md:h-[40vw] flex flex-col justify-center items-start gap-4 p-5 rounded-2xl " alt="" >
+            <img src="assets/pencil-camera.svg" className="absolute left-[50%] translate-[-50%] top-0 w-30" alt="" />
+            <h1 className="text-4xl font-[1000]">brand</h1>
+            <div className="h-1 w-full bg-black"></div>
+            <ul className="flex flex-col gap-2 text-xl">
+              <li>Brand Strategy</li>
+              <li>360 creativity</li>
+              <li>Art Direction</li>
+              <li>Copywriting</li>
+              <li>Editing</li>
+            </ul>
+
+          </div>
+        </div>
+        <div className="cards">
+          <div className=" bg-[#82a0ff] sticky top-40 text-black md:relative md:top-10 -rotate-10 min-w-[15rem] min-h-[20rem] md:w-[17rem] lg:w-[15rem] lg:h-[20rem] md:h-[40vw] flex flex-col justify-center items-start gap-4 p-5 rounded-2xl">
+            <img src="assets/phone-in-hand.svg" className="absolute left-[50%] translate-[-50%] top-0 w-30" alt="" />
+            <h1 className="text-4xl font-[1000]">social</h1>
+            <div className="h-1 w-full bg-black"></div>
+            <ul className="flex flex-col gap-2 text-xl">
+              <li>Social Media Strategy</li>
+              <li>Social Media Creative</li>
+              <li>TikTok / Shoots</li>
+              <li>Campaigns</li>
+
+            </ul>
+          </div>
+        </div>
+
+        <div className="cards">
+          <div className=" bg-[#f5693c] sticky top-40 text-white md:relative md:top-0   rotate-10 min-w-[15rem] min-h-[20rem] md:w-[17rem] lg:w-[15rem] lg:h-[20rem] md:h-[40vw] flex  flex-col justify-center items-start gap-4 p-5 rounded-2xl " alt="" >
+            <img src="assets/smile-blue.svg" className="absolute left-[50%] translate-[-50%] top-0 w-30" alt="" />
+            <h1 className="text-4xl font-[1000]">activation</h1>
+            <div className="h-1 w-full bg-black"></div>
+            <ul className="flex flex-col gap-2 text-xl">
+              <li>Activation Strategy</li>
+              <li>Event Planning</li>
+              <li>Art Direction</li>
+              <li>Production</li>
+            </ul>
+
+          </div>
+        </div>
+        <div className="cards">
+          <div className=" bg-[#a0325a] sticky top-40 text-white md:relative md:top-10 -rotate-10 min-w-[15rem] min-h-[20rem] md:w-[17rem] lg:w-[15rem] lg:h-[20rem] md:h-[40vw] flex flex-col justify-center items-start gap-4 p-5 rounded-2xl " alt="" >
+            <img src="assets/wrist-watch.svg" className="absolute left-[50%] translate-[-50%] top-0 w-30" alt="" />
+            <h1 className="text-4xl font-[1000]">production</h1>
+            <div className="h-1 w-full bg-black"></div>
+            <ul className="flex flex-col gap-2 text-xl">
+              <li>Campaign Video</li>
+              <li>Branded Content</li>
+              <li>Social Content</li>
+              <li>Marketing Materials</li>
+            </ul>
+
+          </div>
 
         </div>
-        <div className="bg-[#82a0ff] sticky top-40 text-black md:relative md:top-10 -rotate-10 min-w-[15rem] min-h-[20rem] md:w-[17rem] lg:w-[15rem] lg:h-[20rem] md:h-[40vw] flex flex-col justify-center items-start gap-4 p-5 rounded-2xl">
-          <img src="assets/phone-in-hand.svg" className="absolute left-[50%] translate-[-50%] top-0 w-30" alt="" />
-          <h1 className="text-4xl font-[1000]">social</h1>
-          <div className="h-1 w-full bg-black"></div>
-          <ul className="flex flex-col gap-2 text-xl">
-            <li>Social Media Strategy</li>
-            <li>Social Media Creative</li>
-            <li>TikTok / Shoots</li>
-            <li>Campaigns</li>
+        <div className="cards">
+          <div className=" bg-[#f0befa] sticky top-40 text-black md:relative md:top-0   rotate-10 min-w-[15rem] min-h-[20rem] md:w-[17rem] lg:w-[15rem] lg:h-[20rem] md:h-[40vw] flex flex-col justify-center items-start gap-4 p-5 rounded-2xl " alt="" >
+            <img src="assets/heart.svg" className="absolute left-[50%] translate-[-50%] top-0 w-30" alt="" />
+            <h1 className="text-4xl font-[1000]">partners</h1>
+            <div className="h-1 w-full bg-black"></div>
+            <ul className="flex flex-col gap-2 text-xl">
+              <li>Brand Strategy</li>
+              <li>360 creativity</li>
+              <li>Art Direction</li>
+              <li>Copywriting</li>
+              <li>Editing</li>
+            </ul>
 
-          </ul>
-        </div>
-
-        <div className="bg-[#f5693c] sticky top-40 text-white md:relative md:top-0   rotate-10 min-w-[15rem] min-h-[20rem] md:w-[17rem] lg:w-[15rem] lg:h-[20rem] md:h-[40vw] flex  flex-col justify-center items-start gap-4 p-5 rounded-2xl " alt="" >
-          <img src="assets/smile-blue.svg" className="absolute left-[50%] translate-[-50%] top-0 w-30" alt="" />
-          <h1 className="text-4xl font-[1000]">activation</h1>
-          <div className="h-1 w-full bg-black"></div>
-          <ul className="flex flex-col gap-2 text-xl">
-            <li>Activation Strategy</li>
-            <li>Event Planning</li>
-            <li>Art Direction</li>
-            <li>Production</li>
-          </ul>
-
-        </div>
-        <div className="bg-[#a0325a] sticky top-40 text-white md:relative md:top-10 -rotate-10 min-w-[15rem] min-h-[20rem] md:w-[17rem] lg:w-[15rem] lg:h-[20rem] md:h-[40vw] flex flex-col justify-center items-start gap-4 p-5 rounded-2xl " alt="" >
-          <img src="assets/wrist-watch.svg" className="absolute left-[50%] translate-[-50%] top-0 w-30" alt="" />
-          <h1 className="text-4xl font-[1000]">production</h1>
-          <div className="h-1 w-full bg-black"></div>
-          <ul className="flex flex-col gap-2 text-xl">
-            <li>Campaign Video</li>
-            <li>Branded Content</li>
-            <li>Social Content</li>
-            <li>Marketing Materials</li>
-          </ul>
-
-        </div>
-        <div className="bg-[#f0befa] sticky top-40 text-black md:relative md:top-0   rotate-10 min-w-[15rem] min-h-[20rem] md:w-[17rem] lg:w-[15rem] lg:h-[20rem] md:h-[40vw] flex flex-col justify-center items-start gap-4 p-5 rounded-2xl " alt="" >
-          <img src="assets/heart.svg" className="absolute left-[50%] translate-[-50%] top-0 w-30" alt="" />
-          <h1 className="text-4xl font-[1000]">partners</h1>
-          <div className="h-1 w-full bg-black"></div>
-          <ul className="flex flex-col gap-2 text-xl">
-            <li>Brand Strategy</li>
-            <li>360 creativity</li>
-            <li>Art Direction</li>
-            <li>Copywriting</li>
-            <li>Editing</li>
-          </ul>
-
+          </div>
         </div>
       </div>
       <div className="w-full min-h-screen lg:px-10 py-10 flex flex-col lg:flex-row justify-between items-center gap-20 lg:gap-10 overflow-x-hidden ">
         <div className="w-full lg:w-[50%] h-full lg:h-screen py-40 lg:py-0 px-10 flex justify-center items-center relative">
           <img src="assets/loose-water.svg" className="absolute w-[80%] max-w-[20rem] left-0 top-0 lg:top-20" alt="" />
-          <img src="assets/heart.svg" ref={heaartRef} className={`z-50 absolute w-20 lg:w-40 left-[50] md:left-[30%] lg:left-[40%] bottom-25 lg:bottom-20 scale-animation ${isVisible2? "scale-100": "scale-0"}`} style={{transitionDelay: "0.2s"}} alt="" />
+          <img src="assets/heart.svg" ref={heaartRef} className={`z-50 absolute w-20 lg:w-40 left-[50] md:left-[30%] lg:left-[40%] bottom-25 lg:bottom-20 scale-animation ${isVisible2 ? "scale-100" : "scale-0"}`} style={{ transitionDelay: "0.2s" }} alt="" />
           <img src="assets/double-parabola.svg" className={"absolute w-40 lg:w-80 left-[55%] bottom-10 lg:bottom-20 rotate-50 lg:-rotate-180"} alt="" />
 
 
